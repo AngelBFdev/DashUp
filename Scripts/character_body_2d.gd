9979def723899257ac18fa17db3b5f759ce0bf9d
AngelBFdev <angel.isaacbf@gmail.com>
2024-09-29 19:48:09 -0600

    Created TileMapLayer script
    - Created generate_platform function
      - Match requested pixels with their sprites
    - Set Pixels to match the spritesheet (8)
    - Created _collision_shape_update function
      - Match requested pixels with collision
    - Changed tabs to spaces in character_body_2d.gd

--- Scripts/character_body_2d.gd
+++ Scripts/character_body_2d.gd
@@ -3,15 +3,15 @@ extends CharacterBody2D
 const TELEPORT_HEIGHT = 100
 
 func _input(event: InputEvent) -> void:
-	if is_on_floor() and (event is InputEventMouseButton or event is InputEventScreenTouch) and event.is_pressed():
-		_teleport_up()
+    if is_on_floor() and (event is InputEventMouseButton or event is InputEventScreenTouch) and event.is_pressed():
+        _teleport_up()
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	
-	move_and_slide()
+    # Add the gravity.
+    if not is_on_floor():
+        velocity += get_gravity() * delta
+
+    move_and_slide()
 
 func _teleport_up():
-	position.y -= TELEPORT_HEIGHT
+    position.y -= TELEPORT_HEIGHT
