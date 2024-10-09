9b906479eadf064ad77402f2f35cee093e8a6223
AngelBFdev <angel.isaacbf@gmail.com>
2024-10-08 19:56:38 -0600

    Created Main Scene
    - Created Script to generate platforms in Main Scene
    - Added VisibleOnSceneNotifier in platform

--- Scripts/platform.gd
+++ Scripts/platform.gd
@@ -7,17 +7,18 @@ var last_tile: int
 var total_tiles: int
 var random_tile: int
 var collision_shape: RectangleShape2D
+var is_visible: bool = true
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     randomize()
-    generate_platform()  
-    pass # Replace with function body.
+    generate_platform()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
     pass
 
-func generate_platform(platform_length: int = 65):
+func generate_platform(platform_length: int = 20):
+    clear()
     tiles = platform_length / PIXELS
     last_tile = platform_length % PIXELS
     total_tiles = tiles + 1 if last_tile else tiles
@@ -32,8 +33,11 @@ func generate_platform(platform_length: int = 65):
         else:
             set_cell(Vector2(i, 0), 0, Vector2i(PIXELS - 1, random_tile))
     _update_collision_shape(platform_length)
-
+    
 func _update_collision_shape(platform_length: float):
     collision_shape = $CharacterBody2D/CollisionShape2D.shape
     collision_shape.set_size(Vector2(platform_length, PIXELS))
     $CharacterBody2D/CollisionShape2D.position = Vector2(platform_length,PIXELS)/2
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+    is_visible = false
